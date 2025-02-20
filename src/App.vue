539bc65cff016b45d3339a666af461e1f40f76d8
Caleb Clark <calebjclark@gmail.com>
2025-02-19 21:22:37 -0600

    feat: countdown clock

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,3 @@
-<script setup lang="ts">
-import SubstackIcon from './assets/substack.svg';
-</script>
-
-<template>
-  <div>
-    <div Banner class="pt-[13px] pb-[12px] relative text-center">
-      <a href="https://clarkbyrnes.substack.com/p/argon-is-live" class="text-white/80 hover:text-white px-2">
-        <SubstackIcon class="inline-block h-5 mr-2 relative top-[-1px]" />
-        NEW: Argon Is Now Live On the Mainnet ->
-      </a>
-    </div>
-    <RouterView />
-  </div>
+<template>    
+  <RouterView />
 </template>
-
-<style lang="scss" scoped>
-[Banner] {
-  display: block;
-  box-shadow: inset 0 -1px 0px rgba(0,0,0,0.2);
-  background: #6c0e79;
-  position: relative;
-  &:after {
-    content: '';
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: -1px;
-    height: 1px;
-    background: rgba(255, 255, 255, 0.2);
-  }
-}
-</style>
--- src/components/ChainStatus.vue
+++ src/components/ChainStatus.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="py-3 pr-3 md:pt-5 md:pr-5 whitespace-nowrap text-sm md:text-base text-white flex flex-row justify-end gap-2 md:gap-4 cursor-default">
-    <div class="opacity-80 hidden md:block">Live On</div>
-    <a href="/testnet" class="flex flex-row items-center gap-2" v-tooltip="'This is where we test new features before they are released on Mainnet.'" align="right">
+  <div class="py-2 pr-1 md:pt-5 md:pr-5 whitespace-nowrap text-sm md:text-base text-white flex flex-row justify-end gap-0 md:gap-2 cursor-default">
+    <div class="opacity-80 hidden py-1 md:block">Live On</div>
+    <a Button href="/testnet" class="flex flex-row items-center gap-2"><!-- v-tooltip="'This is where we test new features before they are released on Mainnet.'" align="right">-->
       <div class="rounded-full w-4 h-4 bg-[#8FFF23]/80 border border-black"></div>
       <span class="opacity-80">
         TESTNET
       </span>
     </a>
-    <a href="/mainnet" class="flex flex-row items-center gap-2" v-tooltip="'Argon\'s Mainnet is live and operational. Click to view our Getting Started Guide.'" align="right">
-      <div class="rounded-full w-4 h-4 bg-[#8FFF23] border border-black"></div>
+    <a Button href="/mainnet" class="flex flex-row items-center gap-2"><!-- v-tooltip="'Argon\'s Mainnet is live and operational. Click to view our Getting Started Guide.'" align="right">-->
+      <div class="LoadingPulse rounded-full w-4 h-4 bg-[#8FFF23] border border-black"></div>
       <span class="opacity-100">
         MAINNET
       </span>
@@ -19,3 +19,26 @@
 <script setup lang="ts">
 import vTooltip from '../directives/tooltip';
 </script>
+
+<style lang="scss" scoped>
+.LoadingPulse {
+  opacity: 1;
+  animation: fadeInOut 1s linear infinite;
+}
+
+a[Button] {
+  @apply  hover:bg-[#670C73] px-2 py-1 rounded;
+}
+
+@keyframes fadeInOut {
+  0% {
+    opacity: 0.3;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.3;
+  }
+}
+</style>
