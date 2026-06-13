05106de1e0e37751f8452d58813a7977821e907a
Caleb Clark <calebjclark@gmail.com>
2026-06-13 10:43:42 -0500

    feat: finished responsiveness for Home + Treasury + Operations

--- src/screens/apps/Operations.vue
+++ src/screens/apps/Operations.vue
@@ -1,59 +1,60 @@
 <template>
   <MainLayout class="pb-0">
     <div class="flex flex-col pt-12 md:pt-22 min-h-[calc(100vh-69px)] text-slate-800 relative">
-      <div class="flex flex-col max-w-220 mx-auto pb-120 text-center">
-        <h1 class="text-argon-600/50 font-bold text-2xl tracking-widest uppercase font-serif">
+      <div class="flex flex-col w-11/12 max-w-220 mx-auto pb-52 md:pb-92 xl:pb-120 text-center">
+        <h1 class="text-argon-600/50 font-bold text-xl md:text-2xl tracking-widest uppercase font-serif">
           Operations App
         </h1>
-        <header class="text-xl md:text-7xl leading-tight font-light font-serif mt-2">
-          The Easiest Way to <br /> Start Mining and Vaulting
+        <header class="text-3xl md:text-5xl lg:text-6xl xl:text-7xl leading-tight font-light font-serif mt-2 md:flex flex-col">
+          <span>The Easiest Way to </span>
+          <span>Start Mining and Vaulting</span>
         </header>
 
-        <p class="text-lg px-10 mt-5">
+        <p class="text-base md:text-lg px-0 md:px-10 mt-5">
           Argon Operations requires no expensive hardware and very little technical knowledge. It runs on Mac, Windows, and
           Linux laptops. Security is built into its foundation: it uses no centralized servers, no login accounts, and
           no telemetry or tracking data of any kind. The entire codebase is open-source, decentralized, and designed to keep you fully anonymous.
         </p>
 
-        <div class="flex flex-row items-center justify-center gap-x-5 mt-14">
-          <a :href="stableUrl" class="flex flex-row items-center justify-center gap-3 bg-argon-button border border-argon-800 text-white rounded-md md:text-2xl w-7/12 px-2 py-2 md:py-3 font-bold cursor-pointer hover:bg-argon-button-hover whitespace-nowrap" style="box-shadow: inset 1px 1px 0 rgba(255, 255, 255, 0.5), 1px 1px 0 rgba(255, 255, 255, 1);">
+        <div class="flex flex-col md:flex-row items-center justify-center gap-3 md:gap-5 mt-10 md:mt-14">
+          <a :href="stableUrl" class="flex flex-row items-center justify-center gap-3 bg-argon-button border border-argon-800 text-white rounded-md md:text-xl xl:text-2xl w-full md:w-7/12 px-2 py-2 md:py-3 font-bold cursor-pointer hover:bg-argon-button-hover whitespace-nowrap" style="box-shadow: inset 1px 1px 0 rgba(255, 255, 255, 0.5), 1px 1px 0 rgba(255, 255, 255, 1);">
             <MacIcon v-if="osName === OsName.mac" class="w-7 h-7 relative -top-0.5 inline-block" />
             <WindowsIcon v-else-if="osName === OsName.windows" class="w-7 h-7 relative inline-block" />
             <span>Download Operations for MacOS</span>
           </a>
-          <a href="#all-downloads" class="flex flex-row items-center justify-center gap-3  border border-argon-800 text-argon-600 rounded-md md:text-2xl w-5/12 px-2 py-2 md:py-3 font-bold cursor-pointer whitespace-nowrap hover:bg-argon-300/15" style="box-shadow: inset 1px 1px 0 rgba(255, 255, 255, 0.5), 1px 1px 0 rgba(255, 255, 255, 1);">
+          <a href="#all-downloads" class="flex flex-row items-center justify-center gap-3  border border-argon-800 text-argon-600 rounded-md md:text-xl xl:text-2xl w-full md:w-5/12 px-2 py-2 md:py-3 font-bold cursor-pointer whitespace-nowrap hover:bg-argon-300/15" style="box-shadow: inset 1px 1px 0 rgba(255, 255, 255, 0.5), 1px 1px 0 rgba(255, 255, 255, 1);">
             <span>Other Platforms</span>
             <ArrowDownIcon class="w-5" />
           </a>
         </div>
-        <div class="mt-5 text-md text-center w-full">
+        <div class="mt-5 text-sm md:text-md text-center w-full">
           ( note: <router-link to="/apps" class="underline">you'll need an access code to use this app</router-link> )
         </div>
       </div>
-      <img src="/app-operations.png" alt="Argon Operations App" class="absolute top-full -translate-y-100 w-8/12 left-1/2 -translate-x-1/2" />
+      <img src="/app-operations.png" alt="Argon Operations App" class="absolute top-full -translate-y-40 md:-translate-y-70 xl:-translate-y-96 w-11/12 md:w-8/12 max-w-300 left-1/2 -translate-x-1/2" />
     </div>
 
     <div class="w-full  bg-linear-to-b from-white to-[var(--bg-color)] border-t border-gray-600/50 relative z-10" style="box-shadow: 0 -5px 5px rgba(0, 0, 0, 0.1);">
-      <div class="max-w-220 mx-auto pt-12">
+      <div class="w-11/12 max-w-220 mx-auto pt-6 md:pt-12">
         <h3 class="font-bold mt-3 font-serif text-2xl">Basic System Requirements</h3>
         <p class="mt-3 mb-3">The computing needs for Argon is minimal. Any modern laptop running a major OS should suffice.</p>
-        <ul class="grid grid-cols-3 w-full space-x-5">
-          <li class="border-t border-gray-300 py-5">
+        <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full md:gap-x-5">
+          <li class="border-t border-gray-300 py-4 md:py-5">
             4GB RAM minimum
           </li>
-          <li class="border-t border-gray-300 py-5">
+          <li class="border-t border-gray-300 py-4 md:py-5">
             2GB disk space
           </li>
-          <li class="border-t border-gray-300 py-5">
+          <li class="border-t border-gray-300 py-4 md:py-5">
             Internet connection
           </li>
-          <li class="border-y border-gray-300 py-5">
+          <li class="border-t md:border-y border-gray-300 py-4 md:py-5">
             MacOS 12+
           </li>
-          <li class="border-y border-gray-300 py-5">
+          <li class="border-t md:border-y border-gray-300 py-4 md:py-5">
             Windows 10+
           </li>
-          <li class="border-y border-gray-300 py-5">
+          <li class="border-y border-gray-300 py-4 md:py-5">
             Ubuntu 20.04+
           </li>
         </ul>
